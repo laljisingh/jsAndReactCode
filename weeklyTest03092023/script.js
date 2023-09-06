4badc099e9ccbd5a39ff6c6c5dbb1c0478ce2efe
laljisingh <lalji7411@gmail.com>
2023-09-06 12:37:41 +0530

    image added

--- weeklyTest03092023/script.js
+++ weeklyTest03092023/script.js
@@ -1,47 +1,39 @@
 const btn = document.getElementById("search-button");
 const hotels = [];
 
-
 btn.addEventListener("click", () => {
-
   const url = fetch("https://indianhotels.free.beeceptor.com/gethotels");
   url
     .then((ele) => {
       return ele.json();
     })
     .then((data) => {
-      data.map((dt) => {  
+      data.map((dt) => {
         hotels.push(dt);
       });
       return data;
     });
 
-
-
   const result_bar = document.getElementById("right");
-  const card = document.createElement("div");
-  card.classList.add = "card";
-
-  hotels.map((data)=>{
-
+  
+  hotels.map((data) => {
+    const card = document.createElement("div");
+    card.classList.add("cards");
     card.innerHTML = `
-    <div class="row g-0">
+                <div class="row g-0">
                    <div class="col-md-4">
                      <img src='${data.image}' class="img-fluid rounded-start" alt="...">
                    </div>
                    <div class="col-md-8">
                      <div class="card-body">
-                       <h5 class="card-title">Happy Hotels</h5>
-                       <p class="card-text">In this hotels All Service are good</p>
-                       <p>Rating : 5 star</p>
-                       <p>Price : 25000rs</p>
-                       <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+                       <h5 class="card-title">${data.name}</h5>
+                       <p class="card-text">Descreption : ${data.description}</p>
+                       <p>Price : ${data.price}rs</p>
                      </div>
                    </div>
                  </div>
                  `;
                  result_bar.appendChild(card);
   });
-                 
- 
 });
+
